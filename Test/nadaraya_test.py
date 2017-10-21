ba1194fa3f453ac8979d2088315cb401b031655d
Andrea Mascaretti <a.mascaretti@campus.fct.unl.pt>
2017-10-21 13:39:00 +0100

    Testing Nadaraya

--- Test/nadaraya_test.py
+++ Test/nadaraya_test.py
@@ -26,26 +26,32 @@ def nad_wat(K, h, X, Y, x):
         num = num + yf * k
     return num/den
 
-
+def nad_wat_predict(K, h, X_train, Y_train, X_test):
+    y_predict= np.zeros(*X_test.shape)
+    for i in range(len(X_test)):
+        y_predict[i]= nad_wat(K, h, X_train, Y_train, X_test[i])
+    return y_predict
+    
+    
 xs= np.random.rand(200) * 10
 ys= f(xs) + 2*np.random.randn(*xs.shape)
 grid= np.r_[0:10:512j]
 
-new_grid= np.zeros(*grid.shape)
-for i in range(len(grid)):
-    new_grid[i]= nad_wat(gaussiank, 0.5, xs, ys, grid[i])
+#new_grid= np.zeros(*grid.shape)
+#for i in range(len(grid)):
+#    new_grid[i]= nad_wat(gaussiank, 0.5, xs, ys, grid[i])
 
-first_grid= np.zeros(*grid.shape)
-for i in range(len(grid)):
-    first_grid[i]= nad_wat(gaussiank, 1, xs, ys, grid[i])
+#first_grid= np.zeros(*grid.shape)
+#for i in range(len(grid)):
+#    first_grid[i]= nad_wat(gaussiank, 1, xs, ys, grid[i])
     
-second_grid= np.zeros(*grid.shape)
-for i in range(len(grid)):
-    second_grid[i]= nad_wat(gaussiank, 0.01, xs, ys, grid[i])
+#second_grid= np.zeros(*grid.shape)
+#for i in range(len(grid)):
+#    second_grid[i]= nad_wat(gaussiank, 0.01, xs, ys, grid[i])
 
 plt.plot(grid, f(grid), 'r--', label= 'Reference')
 plt.plot(xs, ys, 'o', alpha= 0.5, label= 'Data')
-plt.plot(grid, new_grid, 'g', label= 'Fitted with 0.5')
-plt.plot(grid, first_grid, 'b', label = 'Fitted with 1')
-plt.plot(grid, second_grid, 'y', label= 'Fitted with 0.01')
+plt.plot(grid, nad_wat_predict(gaussiank, 0.5, xs, ys, grid), 'g', label= 'Fitted with 0.5')
+plt.plot(grid, nad_wat_predict(gaussiank, 1, xs, ys, grid), 'b', label = 'Fitted with 1')
+plt.plot(grid, nad_wat_predict(gaussiank, 0.01, xs, ys, grid), 'y', label= 'Fitted with 0.01')
 plt.legend(loc= 'best')
